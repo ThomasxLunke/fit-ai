8d879d49f3ddba0fd78a090be444f6f3a5768e34
ThomasxLunke <thomas-sidambarom@hotmail.com>
2025-09-29 19:22:19 +0200

    :bug:

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { getSessionCookie } from 'better-auth/cookies'
+// import { getSessionCookie } from 'better-auth/cookies'
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request)
+  // const sessionCookie = getSessionCookie(request)
 
-  if (!sessionCookie) {
-    return NextResponse.redirect(new URL('/sign-in', request.url))
-  }
+  // if (!sessionCookie) {
+  //   return NextResponse.redirect(new URL('/sign-in', request.url))
+  // }
 
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/dashboard', request.url))
